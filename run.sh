2a0cb9876a75ecb52a4c2ca8e720418ce8968dac
Carlos Gonzalez <19charles96@gmail.com>
2017-07-26 11:05:43 +0100

    Check if curl and tar are available

--- run.sh
+++ run.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 main() {
+  if [ -n "$JAVA_HOME" ] ; then
+    if [ ! -x "$JAVAHOME/bin/java" ] ; then
+        echo "ERROR: JAVA_HOME is set to an invalid directory: $JAVA_HOME"
+        exit 1
+    fi
+  else
+    echo 'Maven requires java to work, please ensure Java is installed and JAVA_HOME set correctly'
+    exit 1
+  fi
+
+  if command -v curl ; then
+    echo "curl is required to fetch maven please install it before this step."
+    exit 1
+  fi
+  if command -v tar ; then
+    echo "tar is required to fetch maven please install it before this step."
+    exit 1
+  fi
+
   if [ ! -d "/maven" ]; then
     mkdir /maven
     echo 'Downloading Maven'
@@ -11,6 +30,10 @@ main() {
     rm apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz
 
   else
+    if [ ! -x "/maven/apache-maven-$WERCKER_MAVEN_VERSION/bin/mvn" ] ; then
+        echo "ERROR:  maven was not installed properly"
+        exit 1
+    fi
     echo 'Maven already present'
   fi
 
