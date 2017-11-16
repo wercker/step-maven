8381df49f3259380fcff2b404b93cd3100f7ea9b
Bashkim Isai <bashkim.isai@hogarth-ogilvy.com>
2017-11-16 12:10:30 +0000

    does not install maven if version not requested

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # Copyright 2017, Oracle and/or its affiliates. All rights reserved.
 
-echo "$(date +%H:%M:%S):  Hello from the Maven Wercker Step"
-echo "For information on how to use this step, please review the documentation in the Wercker Marketplace,"
-echo "or visit https://github.com/wercker/step-maven"
+debug "$(date +%H:%M:%S):  Hello from the Maven Wercker Step"
+info "For information on how to use this step, please review the documentation "
+info "in the Wercker Marketplace, or visit https://github.com/wercker/step-maven"
 
 # check that all of the required parameters were provided
 # note that wercker does not enforce this for us, so we have to check
@@ -12,17 +12,7 @@ if [[ -z "$WERCKER_MAVEN_GOALS" ]]; then
   fail "$(date +%H:%M:%S): All required parameters: goals MUST be specified"
 fi
 
-#
-# check if a specific version of maven was requested, otherwise use the latest one we have tested with
-#
-if [[ -z "$WERCKER_MAVEN_VERSION" ]]; then
-  WERCKER_MAVEN_VERSION="3.5.0"
-fi
-
-#
-# check we have everything we need to run Maven
-#
-
+# check java is installed
 if [ -n "$JAVA_HOME" ] ; then
   if [ ! -x "$JAVA_HOME/bin/java" ] ; then
     fail "$(date +%H:%M:%S):  ERROR: JAVA_HOME is set to an invalid directory: $JAVA_HOME"
@@ -31,21 +21,25 @@ else
   fail "$(date +%H:%M:%S):  Maven requires java to work, please ensure Java is installed and JAVA_HOME set correctly"
 fi
 
-# check that curl is installed
-hash curl 2>/dev/null || { fail "$(date +%H:%M:%S):  curl is required to install maven, install curl before this step."; }
+# if a specific version of Maven was requested, attempt to install it
+if [[ ! -z "$WERCKER_MAVEN_VERSION" ]]; then
+  # check that curl is installed
+  hash curl 2>/dev/null || { fail "$(date +%H:%M:%S):  curl is required to install Maven, install curl before this step."; }
 
-# check that tar is installed
-hash tar 2>/dev/null || { fail "$(date +%H:%M:%S):  tar is required, install tar before this step"; }
+  # check that tar is installed
+  hash tar 2>/dev/null || { fail "$(date +%H:%M:%S):  tar is required, install tar before this step"; }
 
-# check that gzip is installed
-hash gzip 2>/dev/null || { fail "$(date +%H:%M:%S):  gzip is required, install gzip before this step"; }
+  # check that gzip is installed
+  hash gzip 2>/dev/null || { fail "$(date +%H:%M:%S):  gzip is required, install gzip before this step"; }
 
-# check that md5sum installed
-hash md5sum 2>/dev/null || { fail "$(date +%H:%M:%S):  md5sum is required to validate the download, please install it before running this step"; }
+  # check that md5sum installed
+  hash md5sum 2>/dev/null || { fail "$(date +%H:%M:%S):  md5sum is required to validate the download, please install it before running this step"; }
 
-if [ ! -d "/maven" ]; then
-  mkdir /maven
-  echo "$(date +%H:%M:%S):  Downloading Maven"
+  if [ ! -d "/maven" ]; then
+    mkdir /maven
+  fi
+
+  debug "$(date +%H:%M:%S):  Downloading Maven"
   curl -O https://www.apache.org/dist/maven/maven-3/$WERCKER_MAVEN_VERSION/binaries/apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz
   curl -O https://www.apache.org/dist/maven/maven-3/$WERCKER_MAVEN_VERSION/binaries/apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz.md5
 
@@ -53,26 +47,24 @@ if [ ! -d "/maven" ]; then
   CHECK2=$(md5sum apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz)
   CHECK1="$CHECK1  apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz"
   if [ "$CHECK1" = "$CHECK2" ] ; then
-    echo "$(date +%H:%M:%S):  checksum matches"
+    debug "$(date +%H:%M:%S):  checksum matches"
   else
     fail "$(date +%H:%M:%S):  checksum does not match"
   fi
 
-  echo "$(date +%H:%M:%S):  Extracting maven "
+  debug "$(date +%H:%M:%S):  Extracting Maven "
   tar -C /maven -zxf apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz
   rm apache-maven-$WERCKER_MAVEN_VERSION-bin.tar.gz*
 
-else
-  if [ ! -x "/maven/apache-maven-$WERCKER_MAVEN_VERSION/bin/mvn" ] ; then
-      fail "$(date +%H:%M:%S):  ERROR:  maven was not installed properly"
-  fi
-  echo "$(date +%H:%M:%S):  Maven already present"
+  # add maven to $PATH
+  export PATH=$PATH:/maven/apache-maven-$WERCKER_MAVEN_VERSION/bin
 fi
 
-export PATH=$PATH:/maven/apache-maven-$WERCKER_MAVEN_VERSION/bin
+# At this point, Maven should be installed
+hash mvn 2>/dev/null || { fail "$(date +%H:%M:%S):  Maven is not installed and no version was specified."; }
 
 #
-# prepare maven command
+# prepare Maven command
 #
 
 if [ "$WERCKER_MAVEN_DEBUG" = "true" ]; then
@@ -99,30 +91,22 @@ else
   SETTINGS="-s $WERCKER_MAVEN_SETTINGS"
 fi
 
-# set the M2_HOME
-# put security-settings.xml in the right place
+# Set the M2_HOME
+# Put security-settings.xml in the right place
 export M2_HOME="/maven/apache-maven-$WERCKER_MAVEN_VERSION"
 export MAVEN_OPTS="$WERCKER_MAVEN_MAVEN_OPTS"
 
-if [[ -z "$WERCKER_MAVEN_SECURITY_SETTINGS" ]]; then
-  # do nothing
-  echo "" > /dev/null
-else
+if [[ ! -z "$WERCKER_MAVEN_SECURITY_SETTINGS" ]]; then
   cp $WERCKER_MAVEN_SECURITY_SETTINGS $M2_HOME/
 fi
 
-# put the local repository into the Wercker Cache directory, so that it
+# Put the local repository into the Wercker Cache directory, so that it
 # would still be available on subsequent runs (unless cache is cleared)
 # keeping this optional because of bug https://github.com/wercker/wercker/issues/139
 if [ "$WERCKER_MAVEN_CACHE_REPO" = "true" ]; then
-  CAHCE_REPO="-Dmaven.repo.local=$WERCKER_CACHE_DIR/.m2"
-else
-  CACHE_REPO=""
+  debug "$(date +%H:%M:%S): using cache repository at $WERCKER_CACHE_DIR/.m2"
+  MAVEN_OPTS="$MAVEN_OPTS -Dmaven.repo.local=$WERCKER_CACHE_DIR/.m2"
 fi
 
-#
-# run the maven command
-#
-mvn $CACHE_REPO $DEBUG $SETTINGS $PROFILES $MAVEN_OPTS $WERCKER_MAVEN_GOALS
-
-
+# Run the Maven command
+mvn $DEBUG $SETTINGS $PROFILES $MAVEN_OPTS $WERCKER_MAVEN_GOALS
